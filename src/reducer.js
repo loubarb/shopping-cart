ffe05b0aa8f8e69ab4fc5199106154b533fd67bd
loubarb <louis.barbati@gmail.com>
2022-04-24 22:25:46 -0400

    Added functions to clear cart and remove individual items

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,13 @@
 const reducer = (state, action) => {
+  if (action.type === 'CLEAR_CART') {
+    return { ...state, cart: [] };
+  }
+  if (action.type === 'REMOVE') {
+    const removeItem = state.cart.filter(
+      (cartItem) => cartItem.id !== action.payload
+    );
+    return { ...state, cart: removeItem };
+  }
   return state;
 };
 
